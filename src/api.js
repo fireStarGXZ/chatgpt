b92fe62738905bfe4f278436cd74f720a580419c
64104061f23fda247c679fa8 <64104061f23fda247c679fa8@devide>
2023-05-19 08:26:16 +0000

    Auto commit

--- src/api.js
+++ src/api.js
@@ -1,2 +1,2 @@
 export const apiKey = process.env.INSCODE_API_KEY;
-export const apiUrl = 'https://test-inscode-api.csdn.net/api/v1/gpt/';
+export const apiUrl = 'https://inscode-api.csdn.net/api/v1/gpt/';
